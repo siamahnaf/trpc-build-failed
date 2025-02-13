a9eef8f854b66f0bf5fb180ef94f32a8bb3dad37
Siam Ahnaf <siamahnaf198@gmail.com>
2025-02-13 10:34:53 +0600

    ssdgf

--- src/components/ClientHM.tsx
+++ src/components/ClientHM.tsx
@@ -3,7 +3,7 @@
 import { trpc } from "@/trpc/client";
 
 const ClientHM = () => {
-    const { data } = trpc.wdgp.list.useQuery();
+    const [data] = trpc.wdgp.list.useSuspenseQuery();
 
     return (
         <div>
